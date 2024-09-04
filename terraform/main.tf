23f110488a1e742cefc50a76e151858a6dcc58fd
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2024-09-04 18:09:31 +0200

    Added infra and ci

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,4 @@
 terraform {
-  backend "gcs" {
-    bucket = "fun7-cts-terraform-state"
-  }
-
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -14,49 +10,19 @@ terraform {
 provider "google" {
   project     = "compact-marker-434520-k0"
   credentials = base64decode(var.gcp_credentials)
+  region      = "europe-central2"
 }
 
-resource "google_cloud_run_v2_service" "cts_north_america" {
-  name     = "cts_north_america-service"
-  location = "us-central1"
-  ingress  = "INGRESS_TRAFFIC_ALL"
+resource "google_storage_bucket" "fun7_cts_terraform_state" {
+  name          = "fun7_cts_terraform_state"
+  location      = "europe-central2"
+  force_destroy = true
+  storage_class = "STANDARD"
 
-  template {
-    containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
-      ports {
-        container_port = 8000
-      }
-    }
+  versioning {
+    enabled = true
   }
+
+  uniform_bucket_level_access = true
 }
 
-resource "google_cloud_run_v2_service" "cts_asia" {
-  name     = "cts_asia-service"
-  location = "asia-east1"
-  ingress  = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
-      ports {
-        container_port = 8000
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service" "cts_europe" {
-  name     = "cts_europe-service"
-  location = "europe-central2"
-  ingress  = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
-      ports {
-        container_port = 8000
-      }
-    }
-  }
-}
